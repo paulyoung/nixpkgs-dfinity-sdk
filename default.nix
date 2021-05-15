ddbc8c9b7039e6e5667e8e5985452165d7d49824
Paul Young <84700+paulyoung@users.noreply.github.com>
2021-05-15 13:24:21 -0700

    Updates for latest stable and beta versions

--- default.nix
+++ default.nix
@@ -31,6 +31,8 @@ let
             "dfx-${version}.tar.gz"
           ];
         };
+        # Use `find $(dfx cache show) -type f -executable -print` on macOS to
+        # help discover what to symlink.
         installPhase = ''
           export HOME=$TMP
           ./dfx cache install
@@ -40,7 +42,7 @@ let
 
           mkdir -p $out/bin
           ln -s $out/cache/dfx $out/bin/dfx
-          ln -s $out/cache/didc $out/bin/didc
+          ln -s $out/cache/ic-ref $out/bin/ic-ref
           ln -s $out/cache/ic-starter $out/bin/ic-starter
           ln -s $out/cache/mo-doc $out/bin/mo-doc
           ln -s $out/cache/mo-ide $out/bin/mo-ide
@@ -56,22 +58,41 @@ let
     system ? builtins.currentSystem
   }: (
     let
-      dfinity-sdk-0_6_4 = makeSdk {
+      dfinity-sdk-0_6_21 = makeSdk {
         inherit acceptLicenseAgreement system;
         systems = {
           "x86_64-darwin" = {
-            sha256 = "1zn6dhc2a7m6bdn06krfqlf17jhh4fy89f22q83dgdypjz8mqiqp";
+            # sha256 = self.stdenv.lib.fakeSha256;
+            sha256 = "0i92rwk5x13q7f7nyrgc896w2mlbk63lkgmlrvmyciwbggjiv4pc";
           };
           "x86_64-linux" = {
-            sha256 = "02adr9ady2i14wwkahdann67cqgcyrclnc1gzr01m9awwkxrklz4";
+            # sha256 = self.stdenv.lib.fakeSha256;
+            sha256 = "06akn065x7vaqy56v5jn551zbw5a0wfxvn13q0hpskm2iwrwrpnb";
           };
         };
-        version = "0.6.4";
+        version = "0.6.21";
+      };
+
+      dfinity-sdk-0_7_0-beta_8 = makeSdk {
+        inherit acceptLicenseAgreement system;
+        systems = {
+          "x86_64-darwin" = {
+            # sha256 = self.stdenv.lib.fakeSha256;
+            sha256 = "19zq8n5ahqmbyp1bvhzv06zfaimxyfgzvanwfkf5px7gb1jcqf0m";
+          };
+          "x86_64-linux" = {
+            # sha256 = self.stdenv.lib.fakeSha256;
+            sha256 = "0nl29155076k23fx1j0zb92cr4p0dh8fk5cnjr67dy3nwlbygh3x";
+          };
+        };
+        version = "0.7.0-beta.8";
       };
     in
+      # https://sdk.dfinity.org/manifest.json
       {
-        latest = dfinity-sdk-0_6_4;
-        "0_6_4" = dfinity-sdk-0_6_4;
+        latest = dfinity-sdk-0_6_21;
+        "0_6_21" = dfinity-sdk-0_6_21;
+        "0_7_0-beta_8" = dfinity-sdk-0_7_0-beta_8;
       }
   );
 in
