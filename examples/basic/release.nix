105b274676cf324fcddcf672ac62d6b22db103af
Paul Young <84700+paulyoung@users.noreply.github.com>
2022-01-21 17:43:48 -0800

    Fix system argument in basic example shell

--- examples/basic/release.nix
+++ examples/basic/release.nix
@@ -3,18 +3,19 @@ let
      overlays = import ./nix/overlays.nix;
    };
 
-  dfinitySdk = pkgs.dfinity-sdk {
-    acceptLicenseAgreement = true;
-    sdkSystem = "x86_64-darwin";
-  };
-
-  shell = { version ? "0.7.0-beta.8", ... }:
-    pkgs.mkShell {
-      nativeBuildInputs = [
-        dfinitySdk.${version}
-        pkgs.nodejs-12_x
-      ];
-    };
+  shell = { version ? "0.7.0-beta.8", system ? "x86_64-darwin" }:
+    let
+      dfinitySdk = pkgs.dfinity-sdk {
+        acceptLicenseAgreement = true;
+        sdkSystem = system;
+      };
+    in
+      pkgs.mkShell {
+        nativeBuildInputs = [
+          dfinitySdk.${version}
+          pkgs.nodejs-12_x
+        ];
+      };
 in
   {
     inherit shell;
