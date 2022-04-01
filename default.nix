d7380a00d47c35dffb0b36f4421388f7cdf16152
Paul Young <84700+paulyoung@users.noreply.github.com>
2022-04-01 02:32:49 -0700

    Don't patch unarchived binaries in GitHub releases

--- default.nix
+++ default.nix
@@ -13,7 +13,7 @@ let
         then "x86_64-darwin"
         else sdkSystem;
 
-      makeVersion = { systems, url, version }: (
+      makeVersion = { dontPatch, systems, url, version }: (
         if !acceptLicenseAgreement then
           error (builtins.concatStringsSep "\n" [
             ""
@@ -63,7 +63,7 @@ let
               mkdir -p $out/bin
 
               for binary in dfx ic-ref ic-starter icx-proxy mo-doc mo-ide moc replica; do
-                ${self.lib.optionalString self.stdenv.isLinux ''
+                ${self.lib.optionalString (dontPatch && self.stdenv.isLinux) ''
                 local BINARY="$CACHE_DIR/$binary"
                 test -f "$BINARY" || continue
                 local IS_STATIC=$(ldd "$BINARY" | grep 'not a dynamic executable')
@@ -87,6 +87,7 @@ let
       makeVersionFromGitHubRelease = { systems, version }:
         makeVersion {
           inherit systems version;
+          dontPatch = true;
           url =  builtins.concatStringsSep "/" [
             "https://github.com"
             "dfinity"
@@ -102,6 +103,7 @@ let
       makeVersionFromManifest = { systems, version }:
         makeVersion {
           inherit systems version;
+          dontPatch = false;
           url =  builtins.concatStringsSep "/" [
             "https://sdk.dfinity.org"
             "downloads"
