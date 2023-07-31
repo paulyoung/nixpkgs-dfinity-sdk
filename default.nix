2c2234d8c7894f37b14f608e8025fa1c189f2e1f
Joachim Breitner <mail@joachim-breitner.de>
2023-08-01 00:04:33 +0200

    Use autoPatchElf

--- default.nix
+++ default.nix
@@ -58,14 +58,8 @@ let
             installPhase = ''
               export HOME=$TMP
 
-              ${self.lib.optionalString self.stdenv.isLinux ''
-              local LD_LINUX_SO=$(ldd $(which iconv)|grep ld-linux-x86|cut -d' ' -f3)
-              local IS_STATIC=$(ldd ./dfx | grep 'not a dynamic executable')
-              local USE_LIB64=$(ldd ./dfx | grep '/lib64/ld-linux-x86-64.so.2')
               chmod +rw ./dfx
-              test -n "$IS_STATIC" || test -z "$USE_LIB64" || patchelf --set-interpreter "$LD_LINUX_SO" ./dfx
-              ''}
-
+              autoPatchelf .
               ./dfx cache install
 
               local CACHE_DIR="$out/.cache/dfinity/versions/${version}"
